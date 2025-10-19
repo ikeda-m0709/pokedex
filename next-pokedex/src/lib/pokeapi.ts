9d7559003e00195017381ffa2a68da19e5467f57
ikeda-m0709 <95paint18lover@gmail.com>
2025-10-19 16:57:57 +0900

    ポケモン詳細カードの表示内容の修正完了

--- next-pokedex/src/lib/pokeapi.ts
+++ next-pokedex/src/lib/pokeapi.ts
@@ -15,9 +15,8 @@ export async function fetchPokemon({ params }: { params: { id: string }}): Promi
     if(!speciesRes.ok) return notFound();
     const speciesDate: PokemonSpeciesDetail = await speciesRes.json();
 
-    const japaneseName = speciesDate.names.find(n => n.language.name === "ja")?.name ?? "不明";
-    const genus = speciesDate.genera.find(g => g.language.name === "ja")?.genus ?? "不明";
-
+    const japaneseName = speciesDate.names.find(n => n.language.name === "ja")?.name ?? data.name;
+    const genus = speciesDate.genera.find(g => g.language.name === "ja")?.genus ?? "分類なし";
 
     //1ポケモンのうち、複数のabilityがあるので、型も配列になる
     const abilities: ProcessedAbility[] = await Promise.all(
@@ -25,11 +24,21 @@ export async function fetchPokemon({ params }: { params: { id: string }}): Promi
             const abilityRes = await fetch(a.ability.url);
             const abilityData = await abilityRes.json();
             const japaneseAbilityName = abilityData.names.find((n: { name:string; language: { name: string }}) => n.language.name === "ja")?.name ?? a.ability.name;
-            const effect = abilityData.effect_entries.find((e: EffectEntry) => e.language.name === "ja")?.effect ?? "効果不明";
+            const effect = abilityData.effect_entries.find((e: EffectEntry) => e.language.name === "ja")?.effect ?? "説明なし";
             return {
                 name: japaneseAbilityName,
                 effect,
             };
+        })
+    );
+
+    //typesの日本語取得
+    const types: string[] = await Promise.all(
+        data.types.map( async (t: PokemonType) => {
+            const typeRes = await fetch(t.type.url);
+            const typeData = await typeRes.json();
+            const japaneseTypeNames = typeData.names.find((n: { name: string; language: { name: string}}) => n.language.name === "ja")?.name ?? t.type.name;
+            return japaneseTypeNames;
         })
     );
 
@@ -38,7 +47,7 @@ export async function fetchPokemon({ params }: { params: { id: string }}): Promi
         name: data.name,
         japaneseName,
         imageUrl: data.sprites.other['official-artwork'].front_default ?? data.sprites.front_default,
-        types: data.types.map((t: PokemonType) => t.type.name),
+        types,
         height: data.height / 10,
         weight: data.weight / 10,
         genus,
