ef78388134d7b54e31f5b44a57c5489715463370
ikeda-m0709 <95paint18lover@gmail.com>
2025-10-19 17:08:51 +0900

    ポケモン詳細カードの編集一時終了

--- next-pokedex/src/app/pokemon/[id]/page.tsx
+++ next-pokedex/src/app/pokemon/[id]/page.tsx
@@ -1,13 +1,12 @@
 import PokemonCard from "@/components/card";
 import { fetchPokemon } from "@/lib/pokeapi"
 
-
-<p>ここが個々のカード詳細</p>
-
 export default async function PokemonPage({ params }: { params: { id: string }}) {
     const pokemon = await fetchPokemon({params});
 
     return (
-        <PokemonCard  pokemon={pokemon}/>
+        <div>
+            <PokemonCard  pokemon={pokemon}/>
+        </div>
     );
 }
