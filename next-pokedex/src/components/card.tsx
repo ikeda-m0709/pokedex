2b303feb285abfed966775be865c9ad7ffc33f8a
ikeda-m0709 <95paint18lover@gmail.com>
2025-10-21 23:07:40 +0900

    簡易ポケモンカード⇒詳細へのリンク修正

--- next-pokedex/src/components/card.tsx
+++ next-pokedex/src/components/card.tsx
@@ -4,11 +4,11 @@ import { Card,  CardContent } from "@/components/ui/card"
 import Link from 'next/link'
 
 export default async function PokemonCard({ pokemon }: {pokemon: ProcessedPokemon}) {
-
+    
     return (
         <Card>
             <main>
-                <Link href="/pokemon/params.id">
+                <Link href={`/pokemon/${pokemon.id}`}>
                 <CardContent>
                     <Image src={pokemon.imageUrl} alt="ポケモン画像" width="300" height="300"/>
                     <p>No.{String(pokemon.id).padStart(3, "0")}</p>
