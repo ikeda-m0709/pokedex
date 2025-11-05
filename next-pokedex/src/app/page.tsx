5c8bab5a765027a32e4fc7f6f53d36a8ed72f36d
ikeda-m0709 <95paint18lover@gmail.com>
2025-11-05 20:36:29 +0900

    CSS（Topページ）

--- next-pokedex/src/app/page.tsx
+++ next-pokedex/src/app/page.tsx
@@ -12,16 +12,16 @@ import Link from 'next/link';
 export default function Home() {
 
   return (
-    <div>
-      <main>
-        <h1 className="text-center">ポケモン図鑑へようこそ</h1>
+    <div  className="min-h-screen flex flex-col">{/*画面の高さと同じ最小高さを設定（100vh）*/}
+      <main className="flex-1">{/*一番下のAPIリンクを画面下に出す設定（自分だけで残りの高さを満たす）② */}
+        <h1 className="text-center text-gray-800 mt-5">ポケモン図鑑へようこそ</h1>
         <p className="text-center">お気に入りのポケモンを探してみよう！</p>
         
-        <div className="grid grid-cols-2 gap-5 max-w-3xl mx-auto">
-          <Card className="">
+        <div className="grid grid-cols-2 gap-5 max-w-3xl mx-auto mt-20">
+          <Card>
             <CardHeader>
               <CardTitle>ポケモン一覧</CardTitle>
-              <CardDescription>すべてのポケモンを一覧で表示します。画像をクリックして詳細を確認できます。</CardDescription>
+              <CardDescription>すべてのポケモンを一覧で表示します。<br />画像をクリックして詳細を確認できます。</CardDescription>
             </CardHeader>
             <CardContent className="text-center">
               <Button><Link href="/pokemon">一覧を見る</Link></Button>
@@ -31,7 +31,7 @@ export default function Home() {
           <Card>
             <CardHeader>
               <CardTitle>ポケモン検索</CardTitle>
-              <CardDescription>名前で検索してお気に入りのポケモンを見つけましょう。日本語で検索できます。</CardDescription>
+              <CardDescription>名前で検索してお気に入りのポケモンを見つけましょう。<br />日本語（ひらがな・カタカナ）で検索できます。</CardDescription>
             </CardHeader>
             <CardContent className="text-center">
               <Button><Link href="/search">検索する</Link></Button>
@@ -39,9 +39,7 @@ export default function Home() {
           </Card>
         </div>
       </main>
-      <footer>
-        <p>このアプリは<a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">PokéAPI</a>を使用しています</p>
-      </footer>
+        <p className="text-center text-gray-700">このアプリは<a className="text-blue-600" href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">PokéAPI</a>を使用しています</p>
     </div>
   );
 }
