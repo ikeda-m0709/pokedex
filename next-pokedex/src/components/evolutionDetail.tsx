d8e2bd395b421a51362d06cd1d5222ab2f414ede
ikeda-m0709 <95paint18lover@gmail.com>
2025-11-08 15:40:23 +0900

    CSS進化系統図ページ（一方向進化のみ）

--- next-pokedex/src/components/evolutionDetail.tsx
+++ next-pokedex/src/components/evolutionDetail.tsx
@@ -4,70 +4,77 @@ import { evolutionStep } from '@/lib/types'
 
 import PokemonCard from '@/components/card'
 
+import { ChevronsDown } from 'lucide-react';
+
+
 
 
 export default async function EvolutionDetailPage({ data }: {data: evolutionStep[]}) {
 
   return (
-    <div className='pb-7 justify-items-center bg-blue-700'>
+    <div className='pb-7 justify-items-center border-2 border-amber-700 rounded-2xl bg-blue-700'>
         <h2 className='text-2xl font-bold'>進化系統図</h2>
-        <div className='flex flex-col space-y-10  bg-amber-400'>
+        <div className='flex flex-col space-y-5  bg-amber-400'>
           {data.map(p => (
             <div className=' justify-items-center bg-emerald-700' key={p.prof.id}>
               {/* ↓の二つの条件分岐は、進化の有無と、進化条件の有無（無ければulを作らない）*/}
               {data.length > 1 ? (
                 p.details.length > 0 &&(
-                <ul className='border-2'>
-                  {p.details.map((d, i) => (
-                    <React.Fragment key={i}> {/*returnだけでなくmap()の時も、複数の要素を並べる時は必ず一つの親要素が必要。だが、ulの中にdivだとおかしいので、こちらでラップする */}
-                      {d.trigger && <li>【進化条件{i+1}（きっかけ：{d.trigger}）】</li>}
-                      {d.item && <li>アイテム：{d.item}</li>}
-                      {d.gender &&(
-                        <li>
-                        {d.gender === 1
-                          ? "性別：メス"
-                          : d.gender === 2
-                          ? "性別：オス"
-                          : "性別：不明"}
-                        </li>
-                      )}
-                      {d.held_item && <li>持ち物：{d.held_item}</li>}
-                      {d.known_move && <li>覚えている技：{d.known_move}</li>}
-                      {d.known_move_type && <li>覚えている技タイプ：{d.known_move_type}</li>}
-                      {d.location && <li>場所：{d.location}</li>}
-                      {d.min_level && <li>レベル：Lv.{d.min_level}以上</li>}
-                      {d.min_happiness && <li>なつき度:{d.min_happiness}以上</li>}
-                      {d.min_beauty && <li>美しさ：{d.min_beauty}以上</li>}
-                      {d.min_affection && <li>なかよし度：{d.min_affection}</li>}
-                      {d.needs_overworld_rain === true && <li>天候：雨</li>}
-                      {d.party_species && <li>手持ち：{d.party_species}</li>}
-                      {d.party_type && <li>手持ちタイプ：{d.party_type}</li>}
-                      {d.relative_physical_stats !== null && (
-                        <li>
-                          {d.relative_physical_stats === 1
-                            ? "ステータス：攻撃 > 防御"
-                            : d.relative_physical_stats === 0
-                            ? "ステータス：攻撃 = 防御"
-                            : "ステータス：攻撃 < 防御"}
-                        </li>
-                      )}
-                      {d.time_of_day !== null && (
-                        <li>
-                          {d.time_of_day === "day" && "時間帯：昼"}
-                          {d.time_of_day === "night" && "時間帯：夜"}
-                        </li>
-                      )}
-                      {d.trade_species && <li>交換相手：{d.trade_species}</li>}
-                      {d.turn_upside_down === true && <li>本体操作：本体を逆さにしてレベルアップ</li>}
-                    </React.Fragment>
-                    ))
-                  }
-                </ul>
+                <div>
+                  <div className='bg-fuchsia-800 mb-5 text-blue-200 justify-items-center'>
+                    <ChevronsDown size={40} />
+                  </div>
+                  <ul className='px-3 py-2 bg-red-400 rounded-2xl'>
+                    {p.details.map((d, i) => (
+                      <React.Fragment key={i}> {/*returnだけでなくmap()の時も、複数の要素を並べる時は必ず一つの親要素が必要。だが、ulの中にdivだとおかしいので、こちらでラップする */}
+                        {d.trigger && <li>【進化条件{i+1}（きっかけ：{d.trigger}）】</li>}
+                        {d.item && <li>アイテム：{d.item}</li>}
+                        {d.gender &&(
+                          <li>
+                          {d.gender === 1
+                            ? "性別：メス"
+                            : d.gender === 2
+                            ? "性別：オス"
+                            : "性別：不明"}
+                          </li>
+                        )}
+                        {d.held_item && <li>持ち物：{d.held_item}</li>}
+                        {d.known_move && <li>覚えている技：{d.known_move}</li>}
+                        {d.known_move_type && <li>覚えている技タイプ：{d.known_move_type}</li>}
+                        {d.location && <li>場所：{d.location}</li>}
+                        {d.min_level && <li>レベル：Lv.{d.min_level}以上</li>}
+                        {d.min_happiness && <li>なつき度:{d.min_happiness}以上</li>}
+                        {d.min_beauty && <li>美しさ：{d.min_beauty}以上</li>}
+                        {d.min_affection && <li>なかよし度：{d.min_affection}</li>}
+                        {d.needs_overworld_rain === true && <li>天候：雨</li>}
+                        {d.party_species && <li>手持ち：{d.party_species}</li>}
+                        {d.party_type && <li>手持ちタイプ：{d.party_type}</li>}
+                        {d.relative_physical_stats !== null && (
+                          <li>
+                            {d.relative_physical_stats === 1
+                              ? "ステータス：攻撃 > 防御"
+                              : d.relative_physical_stats === 0
+                              ? "ステータス：攻撃 = 防御"
+                              : "ステータス：攻撃 < 防御"}
+                          </li>
+                        )}
+                        {d.time_of_day !== null && (
+                          <li>
+                            {d.time_of_day === "day" && "時間帯：昼"}
+                            {d.time_of_day === "night" && "時間帯：夜"}
+                          </li>
+                        )}
+                        {d.trade_species && <li>交換相手：{d.trade_species}</li>}
+                        {d.turn_upside_down === true && <li>本体操作：本体を逆さにしてレベルアップ</li>}
+                      </React.Fragment>
+                      ))
+                    }
+                  </ul>
+                </div>
                 )
               ) : <p>※進化なし</p>}
-              <div className='max-w-60'>
+              <div className='mt-3 text-center max-w-60'>
                 <PokemonCard key={p.prof.id} pokemon={p.prof} />
-                <p className='text-blue-200 text-center'>↓</p>
               </div>
             </div>
           ))}
@@ -75,3 +82,10 @@ export default async function EvolutionDetailPage({ data }: {data: evolutionStep
     </div>
   )
 }
+
+/*
+                <div className=' mt-5 text-blue-200 justify-items-center'>
+                  <ChevronsDown size={40} />
+                </div>
+
+*/
