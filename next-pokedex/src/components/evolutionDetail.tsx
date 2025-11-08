0386505c5b9ae66d7a356c1a42914f72ac1f70bc
ikeda-m0709 <95paint18lover@gmail.com>
2025-11-08 23:55:31 +0900

    CSS（進化系統ページ）レイアウト修正

--- next-pokedex/src/components/evolutionDetail.tsx
+++ next-pokedex/src/components/evolutionDetail.tsx
@@ -11,38 +11,39 @@ export default async function EvolutionDetailPage({ data }: {data: evolutionStep
   const parent = data[0];
   const child = data.slice(1);
 
-  console.log("イーブイ" + child[3]);
+  console.log(parent)
 
   return (
-    <div className='flex flex-col justify-items-center pb-7 border-2 border-amber-700 rounded-2xl bg-blue-700'>
-      <h2 className='text-2xl font-bold'>進化系統図</h2>
+    <div className=' mx-auto max-w-4xl flex flex-col justify-items-center pb-7 border-2 border-orange-200 rounded-2xl bg-white'>
+      <h2 className='text-2xl font-bold text-center'>進化系統図</h2>
 
       {/*1匹目とそれ以降全てのポケモンの箱*/}
       <div className='flex flex-col items-center'>
 
         {/*1匹目のポケモンの箱*/}
-        <div className='bg-amber-400'> {/*進化の起点のみ別表示にし、分岐進化は */}
-          <div className='bg-amber-50  text-center max-w-40'>
+        <div className='mt-2'> {/*進化の起点のみ別表示にし、分岐進化は */}
+          <div className='text-center max-w-40'>
             <PokemonCard key={parent.prof.id} pokemon={parent.prof} />
+            {child.length === 1 && <p className='mt-5 font-bold'>※進化系統なし</p>}
           </div>
         </div>
 
         {/*2匹目以降のポケモンの箱*/}
 
-        {/*child[0].　　　自分の次が分岐でなく（!isBranching）、自分を除き、残りが1匹（child.lengthが===2）の時　※フシギダネ
-          それ以降全てmapでだして、col並べ（※進化条件あり）
+        {/*child[0]. 自分の次が分岐でなく（!isBranching）、自分を除き、残りが1匹（child.lengthが===2）の時　※フシギダネ
+          それ以降全てmapでだして、col並べ
         */} 
         {(!child[0].isBranching && child.length === 2) && (
           child.map(p => (
-            <div className='flex flex-col items-center bg-emerald-700' key={p.prof.id}>
+            <div className='flex flex-col items-center' key={p.prof.id}>
               <div className='flex flex-col items-center'>
-                <div className=' bg-fuchsia-800 mb-5 text-blue-200'>
+                <div className='my-5 text-blue-500'>
                   <ChevronsDown size={40} />
                 </div>
-                <ul className='px-3 py-2 bg-red-400 rounded-2xl'>
+                <ul className='px-3 py-2 bg-emerald-100 rounded-2xl'>
                   {p.details.map((d, i) => (
                     <React.Fragment key={i}> {/*returnだけでなくmap()の時も、複数の要素を並べる時は必ず一つの親要素が必要。だが、ulの中にdivだとおかしいので、こちらでラップする */}
-                      {d.trigger && <li>【進化条件{i+1}: {d.trigger}】</li>}
+                      {d.trigger && <li className='font-bold mb-1'>【進化条件{i+1}: {d.trigger}】</li>}
                       {d.item && <li>アイテム：{d.item}</li>}
                       {d.gender &&(
                         <li>
@@ -96,20 +97,19 @@ export default async function EvolutionDetailPage({ data }: {data: evolutionStep
 
         {/*child[0].　　　自分の次が分岐（isBranching）の時　※ラルトス
           自分をcolで出し、※1匹目と同じ
-          次をmapでだしてrow並べ（※進化条件あり）
+          次をmapでだしてrow並べ
         */} 
-
         {child[0].isBranching && (
         <div> 
-          <div className='flex flex-col items-center bg-emerald-700' key={child[0].prof.id}>
+          <div className='flex flex-col items-center' key={child[0].prof.id}>
             <div className='flex flex-col items-center'>
-              <div className=' bg-fuchsia-800 mb-5 text-blue-200'>
+              <div className='my-5 text-blue-500'>
                 <ChevronsDown size={40} />
               </div>
-              <ul className='px-3 py-2 bg-red-400 rounded-2xl'>
+              <ul className='px-3 py-2 bg-emerald-100 rounded-2xl'>
                 {child[0].details.map((d, i) => (
                   <React.Fragment key={i}> {/*returnだけでなくmap()の時も、複数の要素を並べる時は必ず一つの親要素が必要。だが、ulの中にdivだとおかしいので、こちらでラップする */}
-                    {d.trigger && <li>【進化条件{i+1}: {d.trigger}】</li>}
+                    {d.trigger && <li className='font-bold mb-1'>【進化条件{i+1}: {d.trigger}】</li>}
                     {d.item && <li>アイテム：{d.item}</li>}
                     {d.gender &&(
                       <li>
@@ -158,17 +158,17 @@ export default async function EvolutionDetailPage({ data }: {data: evolutionStep
             </div>
           </div>
 
-          <div className='flex flex-row'>
+          <div className='flex flex-row gap-10'>
             {child.slice(1).map(p => (
-              <div className='flex flex-col items-center bg-emerald-700' key={p.prof.id}>
+              <div className='flex flex-col items-center' key={p.prof.id}>
                 <div className='flex flex-col items-center'>
-                    <div className=' bg-fuchsia-800 mb-5 text-blue-200'>
+                    <div className='my-5 text-blue-500'>
                       <ChevronsDown size={40} />
                     </div>
-                    <ul className='px-3 py-2 bg-red-400 rounded-2xl'>
+                    <ul className='px-3 py-2 bg-emerald-100 rounded-2xl'>
                       {p.details.map((d, i) => (
                         <React.Fragment key={i}> {/*returnだけでなくmap()の時も、複数の要素を並べる時は必ず一つの親要素が必要。だが、ulの中にdivだとおかしいので、こちらでラップする */}
-                          {d.trigger && <li>【進化条件{i+1}: {d.trigger}】</li>}
+                          {d.trigger && <li className='font-bold mb-1'>【進化条件{i+1}: {d.trigger}】</li>}
                           {d.item && <li>アイテム：{d.item}</li>}
                           {d.gender &&(
                             <li>
@@ -222,23 +222,23 @@ export default async function EvolutionDetailPage({ data }: {data: evolutionStep
 
 
         {/*自分の次が分岐でなく（!isBranching）、自分の前が分岐点（parentIsBranching）の時　※イーブイ
-          それ以降全てmapでだして、row並べ（※進化条件あり）
+          それ以降全てmapでだして、row並べ
         */} 
         {(!child[0].isBranching && child[0].parentIsBranching) && (
-          <div className='flex flex-row flex-wrap justify-center'>
+          <div className='m-5 pb-5 flex flex-row flex-wrap justify-center gap-10 bg-[lab(97_-1.5_0.5)] rounded-2xl'>
             {child.map(p => {
               console.log("進化先:", p.prof.japaneseName, "詳細:", p.details)
 
               return (
-              <div className='flex flex-col items-center bg-emerald-700' key={p.prof.id}>
+              <div className='flex flex-col items-center' key={p.prof.id}>
                 <div className='flex flex-col items-center'>
-                  <div className=' bg-fuchsia-800 mb-5 text-blue-200'>
+                  <div className='my-5 text-blue-500'>
                     <ChevronsDown size={40} />
                   </div>
-                  <ul className='px-3 py-2 bg-red-400 rounded-2xl'>
+                  <ul className='px-3 py-2 bg-emerald-100 rounded-2xl'>
                     {p.details.map((d, i) => (
                       <React.Fragment key={i}> {/*returnだけでなくmap()の時も、複数の要素を並べる時は必ず一つの親要素が必要。だが、ulの中にdivだとおかしいので、こちらでラップする */}
-                        {d.trigger && <li>【進化条件{i+1}: {d.trigger}】</li>}
+                        {d.trigger && <li className='font-bold mb-1'>【進化条件{i+1}: {d.trigger}】</li>}
                         {d.item && <li>アイテム：{d.item}</li>}
                         {d.gender &&(
                           <li>
@@ -289,10 +289,7 @@ export default async function EvolutionDetailPage({ data }: {data: evolutionStep
         </div>
         )}
 
-
       </div>{/*1匹目とそれ以降全てのポケモンの箱の閉じ*/}
-
-
     </div>
   )
 }
