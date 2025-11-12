9f99ddf4dc1b0bf6385070c76bf039af7bbf38c0
ikeda-m0709 <95paint18lover@gmail.com>
2025-11-13 00:02:55 +0900

    検索修正

--- next-pokedex/src/components/search-result copy.tsx
+++ next-pokedex/src/components/search-result copy.tsx
@@ -8,31 +8,31 @@ interface Props {
     query: string;
 }
 
-export async function SearchResult ({ query }: Props) {
-    //ポケモンの日本語名はカタカナのため、ひらがなでの入力をカタカナに変換する
-    const katakanaQuery = query.replace(/[\u3041-\u3096]/g, (char) => (
-        String.fromCharCode(char.charCodeAt(0) + 0x60)
-    ))
+//////////////////※修正前
 
+export async function SearchResult ({ query }: Props) {
     const totalCount = await getTotalPokemonCount();//ポケモンの総件数を取得
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${totalCount}`);//すべてのポケモンのデータを取得
     const data = await res.json();
     const allNames = data.results.map((p: NamedApiResource) => p.name);//すべてのポケモンのnameを配列にする
+    
+    const speciesData: PokemonSpeciesDetail[] = await Promise.all(
+        allNames.map(async (name: string) => { //.mapの時のasyncの位置はここ⇐
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`);//ポケモンの各言語を含んだ情報を総取得
+            if(!res.ok) return null;//見つからなかったらnullを返す
+            return await res.json();//nullが配列に入ってるので、次の下記の処理で排除することから始めないといけない
+        })
+    )
 
-    const matchedPokemons:PokemonSpeciesDetail[] = []; //ここに検索に一致したポケモンの情報を入れていく
-
-    for(const name of allNames) { //1匹ずつ、日本語情報を取得→検索語に一致しているか確認→一致していたらmatchedPokemonsにpushするを繰り返す　※matchedPokemons.lengthが10件まで
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`);//ポケモンの各言語を含んだ情報を総取得
-        if(!res.ok) continue; //上手く処理できなくても次に進む
-        
-        const data = await res.json();//nullが配列に入ってるので、次の下記の処理で排除することから始めないといけない
-        const jaName = data.names.find((n: { name:string; language: { name: string }}) => n.language.name === "ja-Hrkt")?.name;
-
-        if(jaName.includes(katakanaQuery)) {
-            matchedPokemons.push(data);
-            if(matchedPokemons.length >= 10) break;
-        }
-    }
+    //ポケモンの日本語名はカタカナのため、ひらがなでの入力をカタカナに変換する
+    const katakanaQuery = query.replace(/[\u3041-\u3096]/g, (char) => (
+        String.fromCharCode(char.charCodeAt(0) + 0x60)
+    ))
+    
+    const matchedPokemons = speciesData.filter(d => d !== null) //speciesData(全てのポケモンの各言語情報)の配列からnullを排除する
+    .filter(d => d.names.find((d => d.language.name === "ja-Hrkt")) //各言語情報から日本語だけのポケモン情報に絞る
+    ?.name.includes(katakanaQuery)) //全ての日本語情報のポケモンの中に、カタカナに変換後のquery(inputの入力値)が含まれているか
+    .slice(0, 10); //最大10件に制限
     
     const matchedPokemonsdata = await Promise.all( //PokemonCardコンポーネントに渡すために、型をProcessedPokemonにしたいので、不足しているプロパティを取得する
         matchedPokemons.map(async p => {
